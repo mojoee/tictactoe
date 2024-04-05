3e8db9bcf374d66aee0d1b046634df24063925b8
mojoee <47809126+mojoee@users.noreply.github.com>
2024-04-05 13:20:11 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+class Gameboard{
+    constructor(){
+        this.array = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+}
+
+
+class Player{
+    constructor(name){
+        this.score = 0;
+        this.name = name
+    }
+}
